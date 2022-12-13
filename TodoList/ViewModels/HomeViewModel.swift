224fc42d830da1014b9ef709398a35d2b01de2ab
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-13 11:59:31 +0100

    Add addTodo, updateTodo

--- TodoList/ViewModels/HomeViewModel.swift
+++ TodoList/ViewModels/HomeViewModel.swift
@@ -5,4 +5,45 @@
 //  Created by Kaori Persson on 2022-12-13.
 //
 
-import Foundation
+import SwiftUI
+import CoreData
+
+class HomeViewModel: ObservableObject {
+  
+  let container: NSPersistentContainer
+  @Published var savedTodos: [TodoEntity] = []
+  
+  init() {
+    container = NSPersistentContainer(name: "TodoContainer")
+    container.loadPersistentStores { description, error in
+      if let error = error {
+        print("Error loading core data: \(error)")
+      }
+    }
+  } // END: init
+  
+  func fetchTodos() {
+    let request = NSFetchRequest<TodoEntity>(entityName: "TodoEntity")
+    
+    do {
+      savedTodos = try container.viewContext.fetch(request)
+    } catch let error {
+      print("Error fetching: \(error)")
+    }
+  } // END: fetchTodos
+  
+  func updateTodo(entity: TodoEntity) {
+    let currentTitle = entity.title ?? ""
+    let newTitle = currentTitle + "!"
+    entity.title = newTitle
+    saveData()
+  } // END: updateTodo
+  
+  func saveData() {
+    do {
+      try container.viewContext.save()
+    } catch let error {
+      print("Error saving: \(error)")
+    }
+  } // END: saveData
+}
