1d5709d2b466bc162a197016ee6bd356ee094918
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-13 11:48:37 +0100

    Sort files

---
+++ TodoList/ViewModels/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  TodoList
+//
+//  Created by Kaori Persson on 2022-12-13.
+//
+
+import Foundation
---
+++ TodoList/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TodoList
+//
+//  Created by Kaori Persson on 2022-12-13.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
