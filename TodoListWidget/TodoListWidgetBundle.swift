93578ec314254a088748c23ba9ceabd6100b5510
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-13 15:26:48 +0100

    Add widget

---
+++ TodoListWidget/TodoListWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListWidgetBundle.swift
+//  TodoListWidget
+//
+//  Created by Kaori Persson on 2022-12-13.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoListWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TodoListWidget()
+    }
+}
